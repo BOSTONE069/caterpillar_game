98deaa8814811717bb6ff0ec9d86531d464c4dc0
rhemney <bostoneochieng69@gmail.com>
2022-02-27 17:49:01 -0800

    updated version

--- caterpillar.py
+++ caterpillar.py
@@ -22,8 +22,8 @@ leaf.hideturtle()
 leaf.speed()
 
 
-text_turtle = False # this is for defining the text element in the project
-text_turtle = t.Turtle()
+game_started = False
+text_turtle = t.Turtle()# this is for defining the text element in the project
 text_turtle.write('Press SPACE to start', align='center', font=('Arial',18,'bold')) #this is about defining the elements of the text in the program
 text_turtle.hideturtle()
 
@@ -45,7 +45,7 @@ def outside_window(): # this is defined function for the area of the caterpillar
 def place_leaf(): #This function for determining the postion of the leaf
     leaf.hideturtle()
     leaf.setx(rd.randint(-200,200))
-    lead.sety(rd.randint(-200,200))
+    leaf.sety(rd.randint(-200,200))
     leaf.showturtle()
 
 def game_over(): #This function is supposed to show game over
@@ -55,13 +55,63 @@ def game_over(): #This function is supposed to show game over
     t.hideturtle()
     t.write('GAME OVER !', align='center', font=('Arial',30,'normal'))
 
-def display_score(): #This function shows the score in the game
+def display_score(current_score): #This function shows the score in the game
     score_turtle.clear()
     score_turtle.penup()
     x = (t.window_width()/2) - 50
     y = (t.window_height()/2) - 50
+    score_turtle.setpos(x,y)
     score_turtle.write(str(current_score), align='right', font=('Arial',40,'bold'))
 
 
+def start_game():
+    global game_started
+    if game_started:
+        return
+    game_started = True
+    score = 0
+    text_turtle.clear()
 
-ti.sleep(10)
+    caterpillar_speed = 2
+    caterpillar_length = 3
+    caterpillar.shapesize(1,caterpillar_length,1)
+    caterpillar.showturtle()
+    display_score(score)
+    place_leaf()
+
+    while True:
+        caterpillar.forward(caterpillar_speed)
+        if caterpillar.distance(leaf) < 20:
+            place_leaf()
+            caterpillar_length = caterpillar_length + 1
+            caterpillar.shapesize(1,caterpillar_length,1)
+            caterpillar_speed = caterpillar_speed + 1
+            score = score + 10
+            display_score(score)
+        if outside_window():
+            game_over()
+            break
+
+def move_up():
+    if caterpillar.heading() == 0 or caterpillar.heading() == 180:
+        caterpillar.setheading(90)
+
+def move_down():
+    if caterpillar.heading() == 0 or caterpillar.heading() == 180:
+        caterpillar.setheading(270)
+
+def move_left():
+    if caterpillar.heading() == 90 or caterpillar.heading() == 270:
+        caterpillar.setheading(180)
+
+def move_right():
+    if caterpillar.heading() == 90 or caterpillar.heading() == 270:
+        caterpillar.setheading(0)
+
+t.onkey(start_game,'space')
+t.onkey(move_up,'Up')
+t.onkey(move_right,'Right')
+t.onkey(move_down,'Down')
+t.onkey(move_left,'Left')
+t.listen()
+t.mainloop()
